5fbf9f3989e094ceb93b5bd45d9291fca060b3af
Wilmer <wilmernorling@gmail.com>
2022-11-23 21:56:38 +0100

    finished

--- index.js
+++ index.js
@@ -6,17 +6,46 @@ const zip = document.getElementById("zip")
 const submitBtn = document.getElementById("submit-btn")
 
 myInputs = [password, passwordCon, mail, country, zip]
+let activeHelp = false
+
+submitBtn.addEventListener("click", function(e){
+    myInputs.forEach(element => {
+        if(element == passwordCon){
+            if(passwordCon.value != password.value){
+                errorMessage(element)
+                e.preventDefault()
+            }
+        }else if(element.validity.valid){
+            removeError(element)
+            element.nextElementSibling.textContent = ""
+        }else{
+            errorMessage(element)
+            e.preventDefault()
+        }    
+    });
+}) 
+//---active help after trying to submit---///
 
 myInputs.forEach(element => {
-        element.addEventListener("input", function(){
-            if(element.validity.valid){
+    element.addEventListener("input", function(){
+        if(element == passwordCon){
+            if(passwordCon.value == password.value){
                 removeError(element)
                 element.nextElementSibling.textContent = ""
+                passwordCon.style.border = "1px solid #294936"
             }else{
                 errorMessage(element)
-            } 
-        })
+                passwordCon.style.border = "1px solid red"
+            }
+        }else if(element.validity.valid){
+            removeError(element)
+            element.nextElementSibling.textContent = ""
+        }else{
+            errorMessage(element)
+        }   
+    })
 });
+//---active help after trying to submit---///
 
 
 function errorMessage(type){
@@ -39,16 +68,25 @@ function errorMessage(type){
         if(zip.validity.patternMismatch){
             displayError(zip)
             zip.nextElementSibling.textContent = "Five numbers"
+        }else if(zip.validity.valueMissing){
+            displayError(zip)
+            zip.nextElementSibling.textContent = "Five numbers thx :)"
         }
     }else if(type == password){
         if(password.validity.patternMismatch){
             displayError(password)
             password.nextElementSibling.textContent = "Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+        }else{
+            displayError(password)
+            password.nextElementSibling.textContent = "Enter a password"
         }
+    }else if(type == passwordCon){
+        displayError(passwordCon)
+        passwordCon.nextElementSibling.textContent = "Must match password"
     }
 }
 submitBtn.addEventListener("click", function(){
-    
+
 })
 
 function displayError(type){
